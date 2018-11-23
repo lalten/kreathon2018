78cebeb9c7e8c6fe5537bd85d369037089b81384
Nikolas Engelhard <engelhard@magazino.eu>
2018-11-23 23:43:04 +0100

    geo poses

--- scripts/location_processing.py
+++ scripts/location_processing.py
@@ -172,6 +172,11 @@ p[91]="51.320091,6.552320"
 p[96]="51.341310,6.599210"
 
 
+for id in p:
+    l = p[id].replace(' ', '')
+    #print "new Container(%i, 0, 0, new Coordinate(%s), 'asd')," % (id, l)
+    print str(id) + ':' + str(l) + ':' + locations[id]
+
 # ofile = file('poses.txt', 'aw')
 #
 #
