85440a5b5f10d14fb279739dbbd5840ac6f02d63
Laurenz <git@laure.nz>
2018-11-24 01:30:53 +0100

    fill up fake garbage items randomly

--- scripts/send_missing_mqtt_nodes.py
+++ scripts/send_missing_mqtt_nodes.py
@@ -3,7 +3,9 @@
 
 from __future__ import division
 
+import random
 import time
+import numpy as np
 import paho.mqtt.client as mqtt
 
 
@@ -16,15 +18,20 @@ class SendMissingMqttNodes:
         self.client = mqtt.Client()
         self.client.connect(SendMissingMqttNodes.broker_url, SendMissingMqttNodes.broker_port)
         self.client.loop_start()
+        self.fill_level = np.zeros((101,1))
 
     def spin(self):
         try:
             while True:
                 for sensor_id in range(5, 101):
-                    fill_level = 0
+                    self.fill_level[sensor_id] += 10 * random.random()
+                    self.fill_level[sensor_id] = min(self.fill_level[sensor_id], 1000)
+                    if self.fill_level[sensor_id] == 1000:
+                        self.fill_level[sensor_id] = 0  # just reset to zero for now
                     range_status = 0
                     bat_mv = -1
-                    payload_str = '{}_{}_{}_{}\n'.format(sensor_id, fill_level, range_status, bat_mv)
+                    this_fill_level = int(round(self.fill_level[sensor_id]))
+                    payload_str = '{}_{}_{}_{}\n'.format(sensor_id, this_fill_level, range_status, bat_mv)
                     self.client.publish(SendMissingMqttNodes.measurement_topic, payload_str)
                 time.sleep(5)
         except KeyboardInterrupt:
